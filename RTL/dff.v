8f3e9ae1344c6b05fd975ba1ad9a968652505f6b
Bharath <bharathwajmanohar2480@gmail.com>
2025-12-08 11:14:17 +0530

    Update dff.v

--- RTL/dff.v
+++ RTL/dff.v
@@ -1,3 +1,7 @@
+// © 2025 bradator1922 (Bharath). All rights reserved.
+// Provided for learning and modification only.
+// Reuse or submission of this architecture, or any substantially similar derivative, is strictly prohibited without written permission.
+
 module dff(clk,rst,enable,d,q);
 input [31:0] d;
 input clk,rst,enable;
@@ -10,3 +14,4 @@ always@(posedge clk or posedge rst)
         q<=d;   
     end
 endmodule 
+
