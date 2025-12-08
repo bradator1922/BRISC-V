fdab7c6c487c309faf2de674920e3d5b8958e311
Bharath <bharathwajmanohar2480@gmail.com>
2025-12-08 11:14:45 +0530

    Update imem.v

--- RTL/imem.v
+++ RTL/imem.v
@@ -1,3 +1,7 @@
+// © 2025 bradator1922 (Bharath). All rights reserved.
+// Provided for learning and modification only.
+// Reuse or submission of this architecture, or any substantially similar derivative, is strictly prohibited without written permission.
+
 module imem (addr,instr_data);
 input [31:0] addr;
 output [31:0] instr_data;
@@ -8,3 +12,4 @@ output [31:0] instr_data;
     assign instr_data = imem[idx];
 
 endmodule
+
