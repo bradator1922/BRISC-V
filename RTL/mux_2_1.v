ca734086fd6eab682890a86419d80580b2b8466a
Bharath <bharathwajmanohar2480@gmail.com>
2025-12-08 11:15:21 +0530

    Update mux_2_1.v

--- RTL/mux_2_1.v
+++ RTL/mux_2_1.v
@@ -1,3 +1,7 @@
+// © 2025 bradator1922 (Bharath). All rights reserved.
+// Provided for learning and modification only.
+// Reuse or submission of this architecture, or any substantially similar derivative, is strictly prohibited without written permission.
+
 module mux_2_1(i0,i1,sel,mux_out);
 input [31:0] i0,i1;
 input sel;
@@ -5,5 +9,6 @@ output [31:0] mux_out;
 
 assign mux_out = sel ? i1 : i0;
 endmodule
+
 
 
